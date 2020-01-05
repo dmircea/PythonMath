345a31379c37a6d0f7feb5ace18319ecc38aba53
dmircea <dmircea4you@gmail.com>
2020-01-04 21:26:28 -0800

    Added WIP container.py , finished few functions , updated README

---
+++ container.py
@@ -0,0 +1,27 @@
+#   This script is still a work in progres and thus lightly tested. Attempt use at
+#   your own risk.
+
+#   This class will keep track of all lists as separate DataSet
+#   objects. It will also keep track of a history of commands
+#   performed globally.
+class DataSetContainer:
+    def __init__(self):
+        pass
+
+#   This class will keep track of one list and its immediate
+#   statistics as well as any operations performed on it.
+class DataSet:
+    def __init__(self):
+        pass
+
+#   This class will store all statistics for a data set.
+#   Statistics include but are not limited to:
+#   [ average, sum, size, mode, median, quartiles, range, standard deviation]
+class Measures:
+    def __init__(self):
+        pass
+
+#    Potential addition. Currently in debate.
+class CommandInterpreter:
+    def __init__():
+        pass
--- helperFunct.py
+++ helperFunct.py
@@ -1,4 +1,5 @@
 #   This file contains the helper functions for the original script.
+#   Please see the top of 'math1Script.py' for more information.
 
 def simpleSort(arr):
     if(len(arr) < 1):
@@ -23,11 +24,14 @@ def simpleSortDec(arr): #   TODO
                 arr[i] = temp
 
 def checkSorted(arr):
-    print("Checking if sorted...")
+    print('Checking if sorted...')
     for i in range(1,len(arr)):
         if(arr[i-1] > arr[i]):
-            print("Not sorted. Sorting...")
+            print('Not sorted. Sorting...')
             return False;
 
-    print("Already sorted!")
+    print('Already sorted!')
     return True;
+
+def mergeSort(arr):
+    pass
